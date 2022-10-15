d5117972438987c4a17613b110cf4c72de499415
Aravindhan-003 <74173522+Aravindhan-003@users.noreply.github.com>
2022-10-15 15:02:52 +0530

    first commit

---
+++ Sample_mavenproject/src/main/java/com/sample_package/MavenDemo.java
@@ -0,0 +1,10 @@
+package com.sample_package;
+
+public class MavenDemo {
+
+	public static void  main(String args[]){
+		System.out.println("hello");
+
+	}
+
+}
---
+++ Sample_mavenproject/src/test/java/com/sample_package/MavenDemotest.java
@@ -0,0 +1,14 @@
+package com.sample_package;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MavenDemotest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
